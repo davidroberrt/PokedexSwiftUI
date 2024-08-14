37406663b2b8f104dff68467742a00ff38e39ada
David Robert <davidrobert.info@gmail.com>
2024-08-13 21:34:23 -0300

    First commit

---
+++ PokedexSwiftUI/PokedexSwiftUI/PokedexSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexSwiftUIApp.swift
+//  PokedexSwiftUI
+//
+//  Created by David Robert on 13/08/24.
+//
+
+import SwiftUI
+
+@main
+struct PokedexSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
